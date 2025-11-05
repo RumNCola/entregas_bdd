19c204f1185221295c1bf911071a0dc7bcde6ac3
RumNCola <jara.fher@gmail.com>
2025-11-05 16:53:13 -0300

    rdi

--- E3/parametros.php
+++ E3/parametros.php
@@ -29,6 +29,7 @@ $carpeta_firmas_limpias             = carpeta_limpios + "firmas/";
 // elemento es el type de la variable, el segundo su longitud máxima y el tercero True si IS NOT
 // NULL, false si no.
 $rdi_persona = array(
+    arrau(integer, false, true), //id
     array(string, 10, true),     //RUT
     array(string, 30, true),     //nombre
     array(string, 30, true),     //apellido
@@ -36,13 +37,35 @@ $rdi_persona = array(
     array(string, 1e9, false),   //correo. el 1e9 es analogo a poner que es varchar(infinito)
     array(integer, 1e9, false),  // telefono, el número minimo es el que puse en el sgdo attr el 1e9
                                  // sirve para que se cumpla la restriccion del valor numérico.
-    array(array('beneficiario', 'titular', ''),-1, false), //tipo
+    array(array('beneficiario', 'titular', ''), false, false), //tipo
     array(array('Staff médico', 'administrativo', 'paciente', '', 'Staff médico, paciente', //rol
-     'administrativo, paciente'), 1e9, false),
+     'administrativo, paciente'), false, false),
     array(string, 30, false),     // especialidad
     array(string, 30, false),     // firma
     array(string, 30, false)
-    )
+);
 
-    
+$rdi_instituciones_salud = array(
+    array(integer, false, true),        // codigo
+    array(string, 30, true),            // nombre
+    array(array('abierta', 'cerrada'),  //tipo de institucion
+    false, false), 
+    array(string, 12, true),            // rut
+    array(string, 1e9, false)           // enlace
+); 
+$rdi_farmacia = array(
+    array(integer, false, true),        // cod igo generico minsal
+    array(string, 100, true),           // nombre
+    array(string, 256, true),           // descripcion
+    array(array('Alimentos', 'Equipamiento', 'Fármacos', 'insumos', 'psicotrópicos', 'Refrigerados
+    ', 'Sueros'), false, false),        // tipo
+    array(integer, 1e9, false),         //codonu
+    array(string, 50, false),           //clasonu
+    array(string, 50, true),            //clasificacion
+    array(array('activo', 'inactivo'), false, false), //estado
+    array(array(0,1), false, false),    //tipo
+    array(integer, 1e9, true)
+);
+
+
 ?>
